911c2739fb68908dd1c23c9aa98e900db91a6245
Jeremy McCormick <jermccormick@gmail.com>
2019-03-03 12:44:30 +0700

    First working version with chained tasks.

--- python/hps/contrib/sim_compare_batch.py
+++ python/hps/contrib/sim_compare_batch.py
@@ -8,6 +8,13 @@ Created on Sun Mar  3 11:34:38 2019
 
 import luigi
 import os, subprocess
+from hps.lcio.event_proc import EventManager
+from hps.contrib.sim_plots import SimPlotsProcessor
+
+"""
+jermc@thinksgiving:/work/slac/projects/sim_compare$ echo $PYTHONPATH
+/sw/root/install/lib:/work/slac/sim/lcio/LCIO-02-12-01/install//python:/work/slac/hps-lcio-tools/python/hps/contrib/:/work/slac/hps-lcio-tools/python/
+"""
 
 class SlicTask(luigi.Task):
     
@@ -15,10 +22,7 @@ class SlicTask(luigi.Task):
     mac = luigi.Parameter(default="slic_gun.mac")
     output_file = luigi.Parameter(default="slicEvents.slcio")
     
-    def run(self):
-
-        #slic -g ./HPS-EngRun2015-Nominal-v3-5-3-fieldmap.lcdd -m slic_gun.mac
-        
+    def run(self):        
         cmd = ['slic', '-g', self.geom, '-m', self.mac]
         print "running " + str(cmd)
         p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
@@ -29,8 +33,27 @@ class SlicTask(luigi.Task):
         
     def output(self):
         return luigi.LocalTarget(self.output_file)
+    
+class SimAnalTask(luigi.Task):
+    
+    plot_file = luigi.Parameter(default="simPlots.root")
+    
+    def run(self):
+        processors = [SimPlotsProcessor("MySimPlotsProcessor", self.output().path)]
+        files = [self.input().path]
+        mgr = EventManager(processors, files)
+        mgr.processEvents() 
+    
+    def output(self):
+        return luigi.LocalTarget(self.plot_file)
+    
+    def requires(self):
+        return SlicTask()
 
-#class SimCompareTask():
+class SimCompareTask(luigi.Task):
+    
+    def requires(self):
+        return SimAnalTask()
 
 if __name__ == "__main__":
     luigi.run()

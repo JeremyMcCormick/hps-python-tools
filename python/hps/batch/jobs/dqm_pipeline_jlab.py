c9712fad03d5cfb3cc63f038febba38f0dc6d04c
Jeremy McCormick <jermccormick@gmail.com>
2019-05-05 12:33:58 +0700

    More debug output.

--- python/hps/batch/jobs/dqm_pipeline_jlab.py
+++ python/hps/batch/jobs/dqm_pipeline_jlab.py
@@ -411,6 +411,8 @@ class UpdateJobStatusTask(luigi.Task):
                 cmd = 'jobstat -j %d' % job[1]
                 logging.debug("Checking status of job %d." % job_id)
                 p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                if p.stdout is None or p.stdout == '':
+                    print('>>>> No output from jobstat')
                 for l in p.stdout:
                     l = l.decode().strip()
                     if l in UpdateJobStatusTask.statuses:
